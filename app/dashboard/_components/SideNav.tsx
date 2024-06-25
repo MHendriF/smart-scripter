94d1d6db8f12dae9bf83711d3adfb7e2579952fa
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-25 10:58:41 +0700

    implement sidebar

---
+++ app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { FileLock, Home, Settings, WalletCards } from "lucide-react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
+
+export default function SideNav() {
+  const MenuList = [
+    { name: "Home", path: "/dashboard", icon: Home },
+    { name: "History", path: "/dashboard/history", icon: FileLock },
+    { name: "Billing", path: "/dashboard/billing", icon: WalletCards },
+    { name: "Setting", path: "/dashboard/setting", icon: Settings },
+  ];
+
+  const pathname = usePathname();
+  useEffect(() => {
+    console.log(pathname);
+  }, [pathname]);
+
+  return (
+    <div className="h-screen p-5 shadow-sm border">
+      <div className="flex justify-center">
+        <Image src="/logo.svg" width={70} height={70} alt="logo" />
+      </div>
+      <hr className="my-3 border" />
+      <div className="mt-3">
+        {MenuList.map((menu) => (
+          <div
+            className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center
+                ${pathname == menu.path && "bg-primary text-white"} `}
+            key={menu.name}
+          >
+            <menu.icon className="h-6 w-6" />
+            <h2 className="text-lg">{menu.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Smart Scripter",
