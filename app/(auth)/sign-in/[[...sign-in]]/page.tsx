83a46dbd8440fd557c996c63195857a6019b688e
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-25 10:03:55 +0700

    implement sign-in, sign-up page

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,6 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return <SignIn />;
+}
+
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,6 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
+
