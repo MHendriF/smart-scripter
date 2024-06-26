aaed15bd65e5663f4e285fd6125fdae0e80759c7
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-26 12:35:16 +0700

    update ui

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function TemplateCard(item: TemplateProps) {
   return (
     <Link href={`/dashboard/content/${item?.slug}`}>
-      <div className="p-5 shadow-md border rounded-md bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all">
+      <div className="p-5 h-[32vh] shadow-md border rounded-md bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 transition-all">
         <Image src={item.icon} alt={item.name} width={50} height={50} />
         <h2 className="font-medium text-lg">{item.name}</h2>
         <p className="text-gray-500 line-clamp-3">{item.desc}</p>
--- app/dashboard/billing/_components/PlanItemCard.tsx
+++ app/dashboard/billing/_components/PlanItemCard.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { UserSubscriptionContext } from "@/providers/UserSubscriptionContext";
 import { useUser } from "@clerk/nextjs";
+import { Loader2Icon } from "lucide-react";
+import { useContext, useState } from "react";
+
+import { Button } from "@/components/ui/button";
 
 export default function PlanItemCard(props: any) {
   const { plan } = props;
   const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+  const { userSubscription, setUserSubscription } = useContext(UserSubscriptionContext);
+
+  const handleSubscription = () => {
+    setIsLoading(true);
+    // axio.post("/api/create-subscription", {}).then(
+    //   (resp) => {
+    //     console.log(resp.data);
+    //     OnPayment(resp.data.id);
+    //   },
+    //   (error) => {
+    //     setIsLoading(false);
+    //   },
+    // );
+  };
 
   return (
     <div className="rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
@@ -29,7 +49,16 @@ export default function PlanItemCard(props: any) {
         ))}
       </ul>
 
-      {plan.isActive ? (
+      <Button
+        disabled={isLoading}
+        onClick={() => handleSubscription()}
+        className="w-full rounded-full mt-5 p-6"
+        variant="outline">
+        {isLoading && <Loader2Icon className="animate-spin" />}
+        {userSubscription ? "Active Plan" : "Get Started"}
+      </Button>
+
+      {/* {plan.isActive ? (
         <a
           href="#"
           className="mt-8 block rounded-full border cursor-not-allowed border-gray-700 bg-gray-600 px-12 py-3 text-center text-sm font-medium text-white">
@@ -42,7 +71,7 @@ export default function PlanItemCard(props: any) {
           className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600 focus:outline-none focus:ring active:text-indigo-500">
           Get Started
         </a>
-      )}
+      )} */}
     </div>
   );
 }
